77b31e83c72a728c5c4f9b39424c1bc2ae922328
Amardeep <vermaamardeep86@gmail.com>
2024-03-13 22:04:17 +0530

    Fundamentals

--- Fundamentals-2/arrays.js
+++ Fundamentals-2/arrays.js
@@ -1,3 +1,4 @@
+/*
 const friend1 = 'Michal';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -43,3 +44,59 @@ console.log(age3);
 
 const ages = [calcAge(year[0]),calcAge(year[1]),calcAge(year[2])];
 console.log(ages);
+
+
+
+//   const cars = ['Saab','Vovlo','BMW'];
+
+// spaces and line break  are not important . A declaration can span multiple lines:
+//ex --
+// const cars = [
+//     'Saab',
+//     'Volvo',
+//     'BMW',
+// ];
+
+// we can also create an array , and then provide the elements
+
+const cars = [];
+cars[0] = 'Saab';
+cars[1] = 'Volvo';
+cars[2] = 'BMW';
+
+// using the javascript keyword NEW
+
+const cars1 = new Array ('Saab','Volvo', 'BMW');
+
+*/
+
+
+// basic array operation (methods)
+
+const friends = ['Michael','Steven' , 'Peter'];
+friends.push('Jay'); // here push is a function 
+console.log(friends); // jay will be added to the friends array
+
+friends.unshift('John'); // unshift allows you to add one or more elements to the beginning of an array
+console.log(friends); 
+
+// remove elements
+friends.pop(); // remove the last element of array
+console.log(friends);
+const popped = friends.pop();
+console.log(popped); // peter (because peter is going to be removed)
+console.log(friends);
+
+friends.shift(); // removes first element of the array 
+console.log(friends);
+
+console.log(friends.indexOf('Steven')); // return index place of steven that is 1
+
+
+console.log(friends.includes('Steven')); // true because steven is present in array
+console.log(friends.includes('Arthur')); // false because arthur is not present in array
+if(friends.includes('Steven')){
+    console.log('you have a friend called Steven');
+}else{
+    console.log('No friend named Steven ');
+}
---
+++ Fundamentals-2/objects.js
@@ -0,0 +1,114 @@
+/* In js , almost everything is an object 
+
+-- Booleans can be object (if defined with new keyword)
+-- Numbers can be object (if defined with new keyword)
+-- Strings can be object (if defined with new keyword)
+-- Dates are always objects
+-- Maths are always objects
+-- Regular expression are always objects
+-- Arrays are always objects
+-- funtion are always objects
+
+All js values , except primitives, are objects.
+
+A primitive value is a value that has no properties or methods.
+3.14 is a primitive value
+
+
+
+const jonasArray = [
+    'Jonas',
+    1992,
+    'teacher'
+];
+
+const jonasObject = {
+    firstName: 'Jonas',
+    lastName: 'Tideman',
+    DOB : 1992,
+    job : 'teacher'
+};
+console.log(jonasObject);
+
+// dot notation 
+
+console.log(jonasObject.lastName);
+
+// bracket notation
+console.log(jonasObject['lastName']);
+
+const nameKey = 'Name';
+console.log(jonasObject['first' + nameKey]);
+console.log(jonasObject['last' + nameKey]);
+
+*/
+ /* dot notation 
+
+ The dot notation approach involves using a dot or period and a key to access a property.
+
+ syntax -- object.key
+ You have the dot then the key of the property you want to access
+
+const obj = {
+    name : 'Amardeep',
+    age : 21,
+    language : 'Javascript'
+}
+const language = obj.language;
+console.log(obj.language);
+console.log(language);
+
+// we have created language variable because later on the code we can simply call language instead of writing obj.language
+
+
+we can also use dot notation to modify an existing property
+ 
+
+const obj = {
+    name : 'Amardeep',
+    age : 21,
+    language : 'javascript'
+}
+
+obj.age = 100; // here we modified age property
+console.log(obj);
+// {
+//   name: "Amardeep",
+//   age: 100,
+//   language: "javascript"
+// }
+
+
+// also we can add a new property using this approach 
+
+const obj = {
+    name : 'amardeep',
+    age : 21,
+    language : 'javascript'
+}
+obj.location = 'Mercury'; // here we added location property
+console.log(obj);
+ 
+output  {
+      name: "deeecode",
+      age: 80,
+      language: "javascript",
+      location: "Mercury"
+    }
+    */
+
+    // objects methods
+
+// const rishu = {
+//     firstName : 'Rishu',
+//     lastName : 'Verma',
+//     birthYear : 2003,
+//     job : 'student',
+//     friends : ['Michael','Peter','Steven'],
+//     hasDriverLicence : false,
+
+//     calcAge: function(birthYear){
+//         return 2024-birthYear;
+//     }
+// };
+ 
