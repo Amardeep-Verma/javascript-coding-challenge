53f3235e1ef6cafdd52dea51246c9d8fea347d7c
Amardeep <vermaamardeep86@gmail.com>
2024-04-02 23:13:47 +0530

    Added

---
+++ DOM/script.js
@@ -0,0 +1,4 @@
+'use strict';
+// DOM - document object model is structed representation of html documents . allows js to access html elements and styles to manipulate them .
+
+console.log(document.querySelector);
